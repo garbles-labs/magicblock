5672f36934af53274ac4e052d9eaff6f30016525
Gabriele Picco <piccogabriele@gmail.com>
2022-11-30 14:08:09 +0000

    Update website

--- src/components/IntroLine.jsx
+++ src/components/IntroLine.jsx
@@ -1,19 +1,21 @@
 export function IntroLine() {
   return (
-      <div className="text-5xl white">
-          Magicblock is a
-          <span className="ml-4 mr-4 inline bg-gradient-to-r from-indigo-200 via-sky-400 to-indigo-200 bg-clip-text font-display tracking-tight text-transparent">
-              Web3
-          </span>
-          mobile studio that bridges the technical gap between
-          <span className="ml-4 mr-4 inline bg-gradient-to-r from-indigo-200 via-sky-400 to-indigo-200 bg-clip-text font-display text-5xl tracking-tight text-transparent">
-              blockchains
-          </span>
-          and the mass
-          <span className="ml-4 inline bg-gradient-to-r from-indigo-200 via-sky-400 to-indigo-200 bg-clip-text font-display text-5xl tracking-tight text-transparent">
-              mobile market
-          </span>
-          .
+      <div className="text-5xl">
+          <p className="flex-wrap break-inside-auto">
+              Magicblock is a
+              <span className="pl-4 pr-4 bg-gradient-to-r from-indigo-200 via-sky-400 to-indigo-200 bg-clip-text font-display text-5xl tracking-tight text-transparent">
+                  Web3
+              </span>
+              mobile studio that bridges the technical gap between
+              <span className="pl-4 pr-4 bg-gradient-to-r from-indigo-200 via-sky-400 to-indigo-200 bg-clip-text font-display text-5xl tracking-tight text-transparent">
+                  blockchain
+              </span> <br/>
+              and the mass
+              <span className="pl-4 bg-gradient-to-r from-indigo-200 via-sky-400 to-indigo-200 bg-clip-text font-display text-5xl tracking-tight text-transparent">
+                  mobile market
+              </span>
+              .
+          </p>
       </div>
 
   )
